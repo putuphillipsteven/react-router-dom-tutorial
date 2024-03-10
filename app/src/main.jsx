f948f8d976bb6f03b86ae011ccf7006d6a8a0cb8
phillip-steven <putu.phillip@gmail.com>
2024-03-10 12:06:10 +0700

    feat(init): initialization

---
+++ app/src/main.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import './index.css';
+import { Root } from './routes/root/container';
+import ErrorPage from './pages/error-pages/container';
+
+const router = createBrowserRouter([
+	{
+		path: '/',
+		element: <Root />,
+		errorElement: <ErrorPage />,
+	},
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+	<React.StrictMode>
+		<RouterProvider router={router} />
+	</React.StrictMode>,
+);
